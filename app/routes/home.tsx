654389bf91cb75f1a4ebdad4c11854d7dff5621d
Cootzy <umastryxsubterror@gmail.com>
2026-02-15 18:13:53 +0700

    Implement Navbar Section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,18 @@
+import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
 
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta({}: Route.MetaArgs) {
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export default function Home() {
-  return <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>;
+	return (
+		<div className="home">
+			<Navbar />
+			<h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+		</div>
+	);
 }
